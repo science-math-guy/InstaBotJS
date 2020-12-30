0610fd91ac2f58c5a50307ac6806676141a5fea7
automated-generation <76541528+automated-generation@users.noreply.github.com>
2020-12-30 23:51:08 +0100

    changes

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-require('dotenv').config()
+require('dotenv').config();
+
+const nodemailer = require('nodemailer');
 
 const puppeteer = require('puppeteer-extra');
 
@@ -23,9 +25,12 @@ const proxies = {
 
 
 (async() => {
+
+  console.log('Starting new process...')
+
   const browser = await puppeteer.launch({
     headless: true,
-    args: ["--no-sandbox"]
+    args: ['--no-sandbox']
   });
   const page = await browser.newPage();
   await page.setViewport({ width: 800, height: 600 });
@@ -33,9 +38,11 @@ const proxies = {
   await page.minimize();
   await page.maximize();
 
-  await page.goto('https://instagram.com/');
+  await page.goto('https://instagram.com/', {
+      "waitUntil" : "networkidle0"
+  });
 
-  console.log('On Instagram.')
+  console.log('On Instagram.');
 
   await page.waitForSelector('#loginForm > div > div:nth-child(1) > div > label > input');
   if ((await page.$('body > div.RnEpo.Yx5HN > div > div > div > div.mt3GC > button.aOOlW.bIiDR')) !== null) {
@@ -43,10 +50,10 @@ const proxies = {
   }
   await page.type('#loginForm > div > div:nth-child(1) > div > label > input', process.env.USERNAME);
   await page.type('#loginForm > div > div:nth-child(2) > div > label > input', process.env.PASSWORD);
-  await page.click('#loginForm > div > div:nth-child(3) > button')
+  await page.click('#loginForm > div > div:nth-child(3) > button');
   await page.waitForNavigation();
 
-  console.log('Logged in.')
+  console.log('Logged in.');
 
   await page.goto('https://www.instagram.com/explore/people/suggested/');
   await page.waitForSelector('#react-root > section > main > div > div.DPiy6.Igw0E.IwRSH.eGOV_._4EzTm.HVWg4 > div > div > div:nth-child(1) > div.Igw0E.rBNOH.YBx95.ybXk5._4EzTm.soMvl');
@@ -54,10 +61,40 @@ const proxies = {
   let i;
   let selector='';
   for(i=1;i<=5;i++){
-      selector=`#react-root > section > main > div > div.DPiy6.Igw0E.IwRSH.eGOV_._4EzTm.HVWg4 > div > div > div:nth-child(${i}) > div.Igw0E.rBNOH.YBx95.ybXk5._4EzTm.soMvl`
+      selector=`#react-root > section > main > div > div.DPiy6.Igw0E.IwRSH.eGOV_._4EzTm.HVWg4 > div > div > div:nth-child(${i}) > div.Igw0E.rBNOH.YBx95.ybXk5._4EzTm.soMvl`;
       await page.click(selector);
-  }
+  };
+
+  // await page.goto('https://www.instagram.com/automated_generation/');
+
+  // await page.waitForSelector('#react-root > section > main > div > ul > li:nth-child(2) > a > span');
 
   await browser.close();
+
   console.log('Done.');
+
 })();
+
+
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.EMAIL_PASSWORD
+    }
+  });
+
+  let mailOptions = {
+    from: process.env.EMAIL,
+    to: process.env.EMAIL,
+    subject: 'Sending Email using Node.js',
+    text: 'How are u bro ?'
+  };
+
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
